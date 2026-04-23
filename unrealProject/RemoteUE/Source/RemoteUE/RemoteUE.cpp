2d642601f20a3fadf7d559743e07ad45d18e186c
SaadBaig <saad.baig@hotmail.com>
2026-04-23 11:07:41 +0200

    Setting up UnrealEngine Project

---
+++ unrealProject/RemoteUE/Source/RemoteUE/RemoteUE.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RemoteUE.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RemoteUE, "RemoteUE" );
